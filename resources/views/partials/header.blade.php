5f277c598770abc84a6844b0253e9e2c611bf0d3
khasmirmb <111484456+khasmirmb@users.noreply.github.com>
2025-02-03 23:19:09 +0800

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Communication</title>
+
+        @vite(['resources/css/app.css','resources/js/app.js'])
+
+    </head>
+    <body class="bg-white min-h-screen m-0 flex flex-col">
