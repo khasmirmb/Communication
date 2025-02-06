db4421b580183f23f462bf2e894dc68d4cc6aa3e
khasmirmb <111484456+khasmirmb@users.noreply.github.com>
2025-02-07 00:03:48 +0800

    Minor Fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,6 +35,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
 
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            '/oauth/callback'
+        ]);
+    })
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
